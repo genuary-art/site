af0cf14b962663a082b2f80f0cec6ba4c2fa4b02
tripzilch <tripzilch@gmail.com>
2022-10-30 13:09:14 +0100

    modificationen

--- logo.js
+++ logo.js
@@ -1,5 +1,5 @@
 // TODO
-// make output canvas
+// clean up code
 
 let start_time;
 log_start=()=>{
@@ -34,18 +34,18 @@ log_end=()=>{
 }
 
 (code=({copyright:piterpasma,max,floor,abs,sin,cos,min,imul,PI}=Math)=>{
-  // PRUTSTRACER (c) 2022 by Piter Pasma
+  // RAYHATCHER (c) 2022 by Piter Pasma
   //
   // commented version
   //
   // featuring some comments and white space
 
-  logo.onclick = _=>{
+  logo.onclick = _=>{ // click to refresh
     localStorage['cache_img'] = '';
     gen_svg();
   }
 
-  show_img = s=>{
+  show_img = s=>{ 
     (im=new Image).src=s;
     im.decode().then(_=>{
       logo.replaceChildren(im);
@@ -54,6 +54,12 @@ log_end=()=>{
 
   gen_svg = _=>{
     // get options from URL
+    // h = page height in mm
+    // lw = line width in mm
+    // d = line density multiplier (smaller is more dense)
+    // bg = 1=include background rect 0=don't include
+    // res = resolution
+    // svg = 1=draw as SVG (for plotters etc) 0=draw as Canvas/PNG
     O={lw:.45,d:.6,h:140,bg:1,res:1e4,svg:0}; // default options
     (new(U=URLSearchParams)(location.search)).forEach((v,k)=>O[k]=v);
     O.seed = seed = O.seed || 'GENUARY2023' + Date.now();
@@ -64,9 +70,9 @@ log_end=()=>{
     H=O.res; // viewbox height
     DR=O.d*(LW=O.lw/M); // dot radius
     V=document.createElement`canvas`;
-    logo.replaceChildren(V); // also deleted previous image
     C=V.getContext`2d`;
     cw=V.width=Y*(ch=V.height=devicePixelRatio*logo.offsetWidth/Y);
+    logo.replaceChildren(V);
     C.strokeStyle='#0a141e';
     C.lineWidth=LW*ch;
     C.fillStyle='#eee8dd';
@@ -80,15 +86,15 @@ log_end=()=>{
     }
 
     // init PRNG
-    S=Uint32Array.of(9,7,5,3);
+    S=Uint32Array.of(9,7,5,3); // dont init to 0
     R=(a=1)=>a*(a=S[3],S[3]=S[2],S[2]=S[1],a^=a<<11,S[0]^=a^a>>>8^(S[1]=S[0])>>>19,S[0]/2**32);
     [...seed+'SOURCERY'].map(c=>R(S[3]^=c.charCodeAt()*23205));
 
-    // maaaaaath
+    // math defs and functions
     TAU=PI*2;
+    PHI=.5+.5*5**.5; // golden ratio
     T=a=>R(a)-R(a); // triangle noise
     F=(N,f)=>[...Array(N)].map(_=>f()); // loop function
-    Fi=(N,f)=>[...Array(N)].map((_,i)=>f(i)); // loop function
 
     L=(x,y)=>(x*x+y*y)**.5; // vec2 length (adapted from: Elements, Euclid 300 BC)
     len3=(x,y,z)=>(x*x+y*y+z*z)**.5; // vec3 length
@@ -100,20 +106,12 @@ log_end=()=>{
     X=([x,y,z],[a,b,c])=>[y*c-z*b,z*a-x*c,x*b-y*a]; // vec3 cross product
     D=([x,y,z=0],[a,b,c=0])=>x*a+y*b+z*c; // vec2/vec3 dot product
     SM=(a,b,x)=>(x=min(max((x-a)/(b-a),0),1),x*x*(3-2*x)); // smoothstep
-    mod=(x,m)=>(x%=m,x<0?x+m:x); // proper modulo (TODO: test speed vs floor)
-    cl=(x,a,b)=>x<a?a:x>b?b:x;
+    SM=(a,b,x)=>(x=(x-a)/(b-a),x=x<0?0:x>1?1:x,x*x*(3-2*x)); // smoothstep
+    cl=(x,a,b)=>x<a?a:x>b?b:x; // clamp
     k=(a,b)=>a>0&&b>0?L(a,b):a>b?a:b; // 2D edge distance function
-    kx=(a,b)=>a<0&&b<0?-L(a,b):a<b?a:b; // 2D edge distance function
-    k3=(a,b,c)=>k(a,k(b,c));
+    k3=(a,b,c)=>k(a,k(b,c)); // 3D edge distance function
 
-    W=(a,b,r)=>-a<r&&-b<r?L(r+a,r+b)-r:a>b?a:b; // "round max"
     box2=(a,b,c)=>(a=abs(a)-c,b=abs(b)-c,a>0&&b>0?L(a,b):a>b?a:b);
-    cbox2=(a,b,c)=>(a=abs(a)-c,b=abs(b)-c,a>b?a:b);
-
-    box3=(a,b,c,d)=>k(abs(a)-d,k(abs(b)-d,abs(c)-d));
-
-    // W=(a,b,r)=>k(a+r,b+r)-r;
-
 
     // init 3D stuff
     Z=1.15; // zoom / FOV
@@ -125,102 +123,88 @@ log_end=()=>{
     rt=N(X(fw,[T(.05),-1,0])); // camera right axis
     up=X(rt,fw); // camera up axis
 
-    [sx,sy]=(([a,d,g],[b,e,h],[c,f,i])=>(t=a*(e*i-h*f)-b*(d*i-f*g)+c*(d*h-g*e),[[(e*i-f*h)/t,(c*h-b*i)/t,(b*f-c*e)/t],[(f*g-d*i)/t,(a*i-c*g)/t,(c*d-a*f)/t]]))(rt,up,fw); // inverse camera matrix
+    [sx,sy]=(([a,d,g],[b,e,h],[c,f,i])=>(t=a*(e*i-h*f)-b*(d*i-f*g)+c*(d*h-g*e),[[(e*i-f*h)/t,(c*h-b*i)/t,(b*f-c*e)/t],[(f*g-d*i)/t,(a*i-c*g)/t,(c*d-a*f)/t]]))(rt,up,fw); // inverse camera matrix, used to convert 3D direction to 2D
 
-    fw=fw.map(v=>v*Z); // pre multiply fwd vector
+    fw=fw.map(v=>v*Z); // pre multiply fwd vector (used later in raytrace fn)
 
-    G=(x,y,t)=>A(cp,N(A(A(fw,rt,Y*x),up,y)),t); // 2D+depth to 3D function
-
-    // some kind of proper noise??
-    const KNUTH = 0x9e3779b1;
-    let NSEED = R(2**32);
+    // minimal value noise function
+    const KNUTH = 0x9e3779b1; // prime number close to PHI*2**32 (used for multiplicative hash)
+    let NSEED = R(2**32); // random 32 bit integer (yes it's an integer)
     ri=(i,j,k)=>(
       // this function returns random numbers based on integer coordinates i,j,k
+      // the coordinates are truncated modulo 1024 (10 bits)
+      // then they are concatenated into a 30 bit number
+      // this number gets XORed with our random 32 bit integer NSEED
+      // we use 32-bit integer multiply (imul) to do a multiplicative hash
       i=imul((((i&1023)<<20)|((j&1023)<<10)|(k&1023))^NSEED,KNUTH),
-      i<<=3+(i>>>29),(i>>>1)/2**31
+      // then we apply the PCG trick and shift the hashed number based on its
+      // top three bits. (important to also shift away the top 3 bits, see PCG paper
+      // https://www.pcg-random.org/paper.html ) 
+      i<<=3+(i>>>29),
+      // finally we shift the sign bit and return a float between 0..1
+      (i>>>1)/2**31
     );
-    no=F(99,_=>R(999)); 
-    // SM=(a,b,x)=>(x=min(max((x-a)/(b-a),0),1),x*x*(3-2*x)); // smoothstep
+    no=F(99,_=>R(999)); // a bunch of random noise offsets
 
-    nr=(x,y,z,s,i,xi=floor(x=x*s+no[i]),
+    // the noise function
+    nr=(x,y,z,s,i, // (x,y,z) = coords, s=scale, i=noise offset 
+          xi=floor(x=x*s+no[i]), // (xi,yi,zi) = integer coords
           yi=floor(y=y*s+no[i+1]),
           zi=floor(z=z*s+no[i+2]))=>(
-      x-=xi,y-=yi,z-=zi,
-      x*=x*(3-2*x),
-      y*=y*(3-2*y),
+      x-=xi,y-=yi,z-=zi, // now (x,y,z) are fractional coords, used for interpolation
+      x*=x*(3-2*x), // apply smoothstep easing function, this makes
+      y*=y*(3-2*y), // the derivative continuous (because it is 0 at grid points)
       z*=z*(3-2*z),
-        ri(xi,yi,zi)*(1-x)*(1-y)*(1-z)
-      + ri(xi,yi,zi+1)*(1-x)*(1-y)*z
-      + ri(xi,yi+1,zi)*(1-x)*y*(1-z)
-      + ri(xi,yi+1,zi+1)*(1-x)*y*z
-      + ri(xi+1,yi,zi)*x*(1-y)*(1-z)
+        ri(xi,yi,zi)*(1-x)*(1-y)*(1-z) // return the interpolated number
+      + ri(xi,yi,zi+1)*(1-x)*(1-y)*z // xi gets multiplied by (1-x)
+      + ri(xi,yi+1,zi)*(1-x)*y*(1-z) // xi+1 gets multiplied by (x)
+      + ri(xi,yi+1,zi+1)*(1-x)*y*z // and same for yi and zi
+      + ri(xi+1,yi,zi)*x*(1-y)*(1-z) // use all 8 combinations
       + ri(xi+1,yi,zi+1)*x*(1-y)*z
       + ri(xi+1,yi+1,zi)*x*y*(1-z)
       + ri(xi+1,yi+1,zi+1)*x*y*z);
 
-    // very very minimal sinus noise
-    // [pa,pb,pc,qa,qb,qc]=F(6,R); // random phases
-    [na,nb]=F(2,_=>N(F(3,T)));nb=N(X(na,nb));nc=N(X(na,nb)); // noise axes
-    PHI=.5+.5*5**.5;
-    g=x=>(x=abs(x-floor(x)-.5)*2)*x*(6-4*x)-1; // fast fake sinus
-    xg=x=>16*(x-=floor(x)+.5)*(.5-abs(x)); // faster? fake sinus TODO: check if actually faster
-    gg=F(99,(p=R(),q=R())=>x=>g(x+p)*(.7+.3*g(x*PHI+q)));
-    nz=(p,i,s=.05,m=.2,a=D(p,na)*s,b=D(p,nb)*s,c=D(p,nc)*s)=>
-      gg[0+i](a+gg[3+i](c*PHI-b)*m) + 
-      gg[1+i](b+gg[4+i](a*PHI-c)*m) + 
-      gg[2+i](c+gg[5+i](b*PHI-a)*m);
-
-    // eight=(x,y)=>{
-    //   let qq = 0,o=0;
-
-    //   if (x<0&&y<0) { [x,y]=[-y,-x]; qq+=6; o^=1}    
-    //   if (y<0) { [x,y]=[-y,x]; qq+=4; o^=1 }
-    //   if (x<0) { [x,y]=[y,-x]; qq+=2; o^=1 }
-    //   if (x>y) { [x,y]=[y,x]; qq+=1; o^=1 }
-    //   y-=32;
-    //   x-=12;
-    //   let a = ([1,2,1,2,1,2,2,1][qq]) * TAU / 8, ca=cos(a), sa=sin(a);
-    //   [x,y] = [x*ca+y*sa, y*ca-x*sa];
-
-    //   return [x,y,qq];
-    // }
+    // box frame SDF based on Inigo Quilez' formula, see https://iquilezles.org/articles/distfunctions/ 
     frm=(x,y,z,b,e,p,q,r)=>(y=abs(y)-b,z=abs(z)-b,min(k3(x=abs(x)-b,q=abs(y+e)-e,r=abs(z+e)-e),k3(p=abs(x+e)-e,y,r),k3(p,q,z)));
-    sabs=x=>(x*x+.5)**.5;
-    ra=T()<0?1:-1;rx=T(35);r2=.5**.5;
-    a=R(TAU);rs=sin(a);rc=cos(a);
-    rid = T()<0?0:8+T(6);
-    rir = max(15+r2*rid,R(30));riw=1+T(.6);
-    frw = .7+T(.3);frr = 9+R(9)+frw;
-    b8d = 9+R(3); b8r = b8d/4+R(2);
-    barfns=[
+
+    // distance function for the object around the logo
+    ra=T()<0?1:-1; // angle left or right
+    rx=T(35);
+    r2=.5**.5; // sin(45°) (xz rotation)
+    a=R(TAU);rs=sin(a);rc=cos(a); // random angle for yz rotation
+    rid = T()<0?0:8+T(6); // x reflection distance, if >0 then there's two of the object
+    rir = max(15+r2*rid,R(30));riw=1+T(.6); // radius and width of the object
+    frw = .7+T(.3);frr = 9+R(9)+frw; // different settings if object is a box frame
+    b8d = 9+R(3); b8r = b8d/4+R(2); // distance and radius for 8-balls object
+    barfns=[ // object distance functions
       (x,y,z)=>L(L(y,z)-rir,abs(x)-rid)-riw, // ring
       (x,y,z)=>box2(box2(y,z,rir),abs(x)-rid,riw)-.3, // square
-      (x,y,z)=>frm(x,y,z,frr,frw), // frame
+      (x,y,z)=>frm(x,y,z,frr,frw), // box frame
       (x,y,z)=>(y=abs(y),z=abs(z),[z,y]=z<y?[z,y]:[y,z],len3(x-6,y-b8d*2,z-b8d)-b8r), // 8 balls
     ];
-    barfn = barfns[R(barfns.length)|0];
-    // twp=R();
-    // barfn=(x,y,z,a=x*.2+twp,s=xg(a),c=xg(a+.25))=>(y-=18,L(y*c+z*s,z*c-y*s)-2);
-    // === DISTANCE FUNCTION ===
+    barfn = barfns[R(barfns.length)|0]; // pick random object
+
+    // === THE BIG DISTANCE FUNCTION FOR EVERYTHING ===
     P=([x,y,z])=>{
-      // let plane = -z+2, bplane = -z+12;
-      // let bar = L(y-10,z-12)-6;
       let by=y-12,bz=rc*z+rs*by;by=rc*by-rs*z;
-      let bar = barfn((x+bz*ra+rx)*r2,by,(x-bz*ra+rx)*r2);
-      // let bar = L(L(y-10,(x-z*ra+rx)*r2)-23,abs(x+z*ra+rx)*r2-8)-.6;
-      let x0,ns=.04;
+      let bar = barfn((x+bz*ra+rx)*r2,by,(x-bz*ra+rx)*r2); // object around logo
+
+      // noise distortion for the letters
+      let ns=.04;
       let n0 = 2*nr(x,y,z,ns,0) + nr(x,y,z,ns*PHI,3);
       let n1 = 2*nr(x,y,z,ns,6) + nr(x,y,z,ns*PHI,9);
       let n2 = 2*nr(x,y,z,ns,12) + nr(x,y,z,ns*PHI,15);
       x+=(n0*2-3)*2.0;
       y+=(n1*2-3)*2.0;
       z+=(n2*2-3)*1.6;
-      let yw = SM(15,3,y);
+      let yw = SM(15,3,y); // makes the letters fatter at the bottom
       let br = .8 + 1 * yw;
-      y -= 4;
-      x += 49; 
-      z -= cl(z,yw-1,1-yw);
-      let ay9=abs(y-9),ay12=abs(y-12),y9=y>9,y6=y>6;
+      y -= 4; // letters base line
+      x += 49; // centre horizontally
+      z -= cl(z,yw-1,1-yw); // make the thinner parts of letters a bit fatter in z-direction
+
+      // distance functions for all the letters -- it's magic
+      let x0,ay9=abs(y-9),ay12=abs(y-12),y9=y>9,y6=y>6;
       x0=(x>0)*3;
       let G = k(min(x-1,y-8),L(k(abs(x)-1-x0,ay9-3-x0)-3+x0,z));
       x -= 12;
@@ -234,13 +218,7 @@ log_end=()=>{
       x -= 12; x0=(x<0)*3;
       let R = k(L(z,k((y9?ay12:y-6)-x0,abs(x)-1-x0)-3+x0),3-y);
       x -= 12; 
-      let Y = L(z, 
-                k(
-                  min(abs(k(-1-x,12-y)-3),abs(k(x-1,-y+1)-3)),
-                  k(y-15,
-                    x-4)
-                  )
-                );
+      let Y = L(z,k(min(abs(k(-1-x,12-y)-3),abs(k(x-1,-y+1)-3)),k(y-15,x-4)));
       x -= 16; x0=(!y6&&x<0)*3;
       let d2a = k(L(z, k(x-1,ay12)-3),-3-x+3*(y<12));
       let d2b = k(L(z, k(-x-1-x0,abs(y-6)-x0)-3+x0),-3+x+3*(y>6));
@@ -248,9 +226,8 @@ log_end=()=>{
       x -= 12;
       let d3 = k(L(z, k(x-1,abs(ay9-3))-3),-3-x);
       let dd = min(G, E, N, U, A, R, Y, d2, d3)-br;
-      // let rin = L(dd-2,(x+96)*rx+(y-9)*ry)-.3;
-      return min(bar,dd);
-    }; // distance function for everything
+      return min(bar,dd); // return distance function for union of letters and object around letters
+    };
 
     Npts=0;
     u=([x,y])=>{ // raytrace function, evaluate point (x,y) on screen
@@ -260,35 +237,18 @@ log_end=()=>{
         d=IX(cp,rd=N(A(A(fw,rt,x),up,y)),150); // trace a ray
         if(d<150){ // did we hit anything
           n=nl(p=A(cp,rd,d)); // calc normal
-          lv=N(A(lp,p,-1));ld=m;
-          // shade=1;//SM(0,.3,edg*edg);
-          la=SM(150,60,ld);
-          shade=.4+.6*la*(ld<150 && IX(A(p,n,.02),lv,ld,.02)>=ld);
-          let ao_step=1.5,aos=0;
-          for(a=0,b=1,i=1;i<6;i++,b*=.7){
-            a+=max(0,b*P(A(p,n,i*ao_step))/i); // shadow ray + AO loop
-            aos+=b*ao_step;
-          }
-          a/=aos;
-          // shade *= a * .2 + .8*max(0,D(n,lv))*(.5+.5*a); // diffuse
-          shade *= max(0,D(n,lv)); // diffuse
-          // shade *= SM(0,.9,max(0,D(n,lv)));
-          edg=(1-cl(-D(n,rd)+.5*n[1]**2,0,1))**2;
-          // shade*=1-edg;//SM(.9,0,edg);
-          shade=shade+(1-shade)*SM(75,150,d);
-          // d*=edg;
-          d=1-cl(shade,0,1);
-
+          lv=N(A(lp,p,-1));ld=m; // lv = light vector, ld = light distance
+          shade=.4+.6*(ld<150 && IX(A(p,n,.02),lv,ld,.02)>=ld); // apply shadow
+          shade *= max(0,D(n,lv)); // diffuse lighting
+          edg=(1-cl(-D(n,rd)+.5*n[1]**2,0,1))**2; // how edgy it is
+          d=1-cl(shade,0,1); // clamp
         } else {
-          d=.5*SM(1,0,rd[2]+.2*rd[1]);
-
-          // d=DR/(d+1e-3);
+          d=.5*SM(1,0,rd[2]+.2*rd[1]); // background gradient
           edg=1;
-          // d=0;
-          n=[0,0,-1];lv=hv;
+          n=[0,0,-1];
         }
         cr=DR/(d+1e-3); // transform grey value 1..0 into clear radius
-        hhv=mix(hv,rd,edg);
+        hhv=mix(hv,rd,edg); // hatch direction
         // hhv=hv;
         return [
           N([D(sx,hd=N(X(n,hhv))),D(sy,hd),0]), // surface hatch direction
@@ -299,7 +259,7 @@ log_end=()=>{
     };
 
     IX=(o,d,z,t=0,h=9)=>{for(;t<z&&h>.005;t+=h=.7*P(A(o,d,t)));return t}; // ray intersect
-    nl=([x,y,z],e=1e-4,l=P([x+e,y,z]),s=P([x,y,z+e]),n=P([x,y+=e,z]),o=P([x+e,y,z+e]))=>N([l-s-n+o,o-l-s+n,o-l+s-n]); // normal
+    nl=([x,y,z],e=1e-4,l=P([x+e,y,z]),s=P([x,y,z+e]),n=P([x,y+=e,z]),o=P([x+e,y,z+e]))=>N([l-s-n+o,o-l-s+n,o-l+s-n]); // normal (tetrahedral method)
 
     // very very tiny QuadTree (spatial data structure to speed up 2D point queries)
     Qa=(q,v,{x,y,w,p}=q)=>p[7]?(w/=2,q.r?0:q.r=[Q(x+w,y+w,w),Q(x+w,y,w),Q(x,y+w,w),Q(x,y,w)],Qa(q.r[(v[0]<x+w)*2+(v[1]<y+w)],v)):p.push(v);
