deedfbcbf20304773c09e589be0a4f38d7ba1fe0
ritz <pseudoscalar@gmail.com>
2024-12-16 11:56:07 +0100

    logo code tidy

--- logo.js
+++ logo.js
@@ -1,5 +1,7 @@
 // Genuary 2025 logo code, Copyright by Piter Pasma
 (code=({copyright:piterpasma,max,floor,abs,sin,cos,min,imul,PI}=Math)=>{
+  // Hello and welcome to the code
+  //
   // featuring some comments and white space
 
   gen_img = _=>{
@@ -113,7 +115,6 @@
     balx = T(299); baly=T(99);
     P=([x,y,z])=>{
       let by=y-12,bz=rc*z+rs*by;by=rc*by-rs*z;
-      let bar = 9999;//barfn((x+bz*ra+rx)*r2,by,(x-bz*ra+rx)*r2); // object around logo
       // noise distortion for the letters
       let ns=.04,nsp=ns*PHI;
       let n0 = 2*nr(x,y,z,ns,0) + nr(x,y,z,nsp,3);
@@ -122,7 +123,6 @@
       x+=(n0*2-3)*2.0;
       y+=(n1*2-3)*2.0;
       z+=(n2*2-3)*1.6;
-      let bal = 9999;//y*21/29-z*20/29+8;
       let yw = SM(15,3,y); // makes the letters fatter at the bottom
       let br = .8 + 1 * yw; // letter thickness
       y -= 4; // letters base line
@@ -130,7 +130,8 @@
       z -= cl(z,yw-1,1-yw); // make the thinner parts of letters a bit fatter in z-direction
 
       // distance functions for all the letters, don't worry it's magic -- I had a hard 
-      // time figuring it out again this year, and I only had to change a 3 to a 4 :)
+      // time figuring it out again this year and the year before, and I only had to change 
+      // a 3 to a 4 into a 5 :)
       let x0,ay9=abs(y-9),ay12=abs(y-12),y9=y>9,y6=y>6;
       x0=(x>0)*3;
       let G = k(x<y-7?x-1:y-8,L(k(abs(x)-1-x0,ay9-3-x0)-3+x0,z)); // G
@@ -153,17 +154,8 @@
       // let d2 = min(d2a,d2b); // digit 2 // d2
       x -= 12; x0=y>12&&x<0?3:0;
       let d5 = min(k(-3-x+3*y6,L(z, k(x-1,abs(6-y))-3)),k(-3+x+3*(y<12),L(z, k(-x-4-x0,ay12-x0-3)+x0)));
-    // p.x -= 16;
-    // x0=p.y<6&&p.x<0?3:0;
-    // GE = U(GE,U(kq2(-3-p.x+y12,z2+sqr(k(p.x-1,ay12)-3)),kq2(-3+p.x+y6,z2+sqrk(-p.x-1-x0,B(p.y-6)-x0)-3+x0)))); // digit 2
-    // p.x -= 12; 
-    // x0=p.y>12&&p.x<0?3:0;
-    // GE=U(GE,U(kq2(-3-p.x+y6,LZ2(z2, k(p.x-1,B(6-p.y))-3)),kq2(-3+p.x+y12,LZ2(z2, k(-p.x-4-x0,ay12-x0-3)+x0))));
-    // return sqrt(GE)-.8-S(11,-1,p.y); // return distance function for union of letters and object 
 
-
-      let letters = min(G, E, U, NAR, Y, d2a,d2b, d5)-br; // union of letters inflated by br
-      return bar<letters&&bar<bal?(ma=1,bar):letters<bal?(ma=0,letters):(ma=2,bal);
+      return min(G, E, U, NAR, Y, d2a,d2b, d5)-br; // union of letters inflated by br
     };
 
     Npts=0;MAXD=150;
@@ -179,20 +171,10 @@
           shade *= max(0,D(n,lv)); // apply diffuse lighting
           // shade *= SM(MAXD,.3*MAXD,d); // depth shading
           d=cl(shade,0,1); // clamp brightness
-          // P(p); // evaluate dist func to get material
-          // cc=d*d>.5+R(.5)?['#ff8','#8ff','#ccc'][ma]:['#fc4','#4cf','#888'][ma];          
         } else {
           d= 0;
-          // cc='#234';
         }
         return d;
-        // rr=.5+d*2;
-        // cr=rr*DR/(d**2*.999+.001); // transform grey value 1..0 into clear radius
-        // return [
-        //   cr<.3&&!Qh(QT,x-cr,y-cr,cr*2,(u,v)=>(x-u)**2+(y-v)**2<cr*cr), // clear test
-        //   cc, // color
-        //   rr // radius
-        //   ]
       }
       return 0; // fail
     };
@@ -212,23 +194,24 @@
       C.moveTo(x + r, y); C.arc(x, y, r, 0, TAU); 
     }
     let poly=pp=>{
+      // Felt this thud, it was a bass line
       let N=pp.length,N1=1/N,ar = 0,j=N-1;
       pp.map(([x,y],i)=>(ar+=x*pp[j][1]-y*pp[j][0],j=i));
-      ar=abs(ar)/2;
+      ar=abs(ar)/2; // area of the polygon using shoe lace method
       j=N-1;let dp=pp.map((p,i)=>H(pp[j],pp[j=i]));
       let cf=0,cd=dp.map(d=>cf+=d);
       let cp=pp.reduce((a,p)=>A(a,p,N1),[0,0]);
       let pt=t=>{
+        // Boom, fuckin' boom, man
         t*=cf;
         let i=0,j=N-1,c,d;
         for(;i<N;j=i++)if((c=cd[i])>t){t=(t-c+(d=dp[i]))/d;break;}
         return [j,mx2(pp[j],pp[i],t)];
       };
       return {
+        // Sorry, dude, I thought you were an object
         ar,cf,dp,cd,pt,pp,
-        cpr2:(i=R(N)|0,j=(i+1+R(N-1)|0)%N)=>mx2(pp[i],pp[j],R()),
-        cpr2o:_=>(a=0,A([0,0],pp.reduce((p,c)=>(a+=b=R(),A(p,c,b)),[0,0]),1/a)),
-        cpr:_=>mx2(cp,pp[R(N)|0],R()),
+        cpr2:(i=R(N)|0,j=(i+1+R(N-1)|0)%N)=>mx2(pp[i],pp[j],R()), // return somewhat random point within polygon
         split:(t0,t1)=>{
           let [i0,p0]=pt(t0);
           let [i1,p1]=pt(t1);
@@ -247,46 +230,45 @@
           let qq=pp.map(p=>A(cp,A(p,cp,-1),a));
           qq.map(([x,y])=>C.lineTo((x+Y2)*ch,(y+.5)*ch));
           C.closePath();
-          // C.fill();
-          // C.stroke();
         }
       };
     }
     function* E() {
       // rendering function
-      // dbglogv=[];
       yield;
       start_time=Date.now();
       // clear
       C.fillStyle='#000';
       C.fillRect(0,0,cw,ch);
-      // console.log([cw,ch]);
       C.lineWidth=cw*.002;
       C.lineJoin=C.lineCap="round";
       let pol=[poly([[-Y2-.1,-.6],[-Y2-.1,.6],[Y2+.1,.6],[Y2+.1,-.6]])];
       let nn=0;
-      for(;pol.length>0;){
+      for(;pol.length>0;){ // let's subdividing
         py=pol.pop();
-        ar=py.ar;r=ar**.5;
-        // nnn=4+ar*8e3|0;
+        ar=py.ar;
+        r=ar**.5; // the square root of the area is the radius, surely
         let rv=cl(r*2.5,0,1);
-        ntest=floor(1+180*rv);
-        // dbglogv.push(ntest);
-        li=max(...F(ntest,_=>lit(py.cpr2())));
-        lr=.005/(li**2+1e-5);
+        ntest=floor(1+180*rv); // how many points we will test
+        li=max(...F(ntest,_=>lit(py.cpr2()))); // test that many points and get lightest point
+        lr=.005/(li**2+1e-5); // turn it into a radius or something, also the **2 is a gamma correct probably
         if((r>lr) && r>.003){
+          // if the "radius" of the polygon (r) is larger than the "radius it should be" (lr)
+          // and it's also not too small (.003) then we subdivide it
+          //
+          // we kinda want to subdivide the polygon somewhat evenly, which I decided means
+          // minimizing the maximum circumference of the two new polygons
+          //
+          // but because I don't wanna figure out how to do that I just try it 32 times
+          // randomly and choose the option with the smallest max circumference
           let be=9e9,b0,b1;
           for(let i=0;i<32;i++){
-            let [p0,p1] = py.split(R(),R()), e = max(p0.cf,p1.cf);
+            let [p0,p1] = py.split(R(),R()), e = max(p0.cf,p1.cf); // max circumference
             if(e<be){be=e;b0=p0;b1=p1}
-          }
+          } // it's fine
           pol.push(b0);
           pol.push(b1);
         } else {
-          // py.draw();
-          // C.strokeStyle='#0fc';
-          // C.lineWidth=cw*.002;
-          // C.stroke();
           py.draw();
           C.fillStyle='#131313';
           C.strokeStyle=`hsl(170 100% ${(15+85*(1-rv)**2).toFixed(2)})`;
@@ -295,14 +277,10 @@
           C.stroke();
         }
         if (++nn>499) {nn=0;yield;} // occasionally yield for the event loop
-        // console.log(nn);
       }
       end_time = Date.now();
       elaps = (end_time - start_time)/1000;
       console.log(`elapsd:${elaps.toFixed(2)}`);
-      // dbglogv.sort((a,b)=>a-b);
-      // console.log(dbglogv.slice(0,9));
-      // console.log(dbglogv.slice(-9));
     }
 
     let J=_=>E.next().done||setTimeout(J); // timeout loop function loops until E iterator is done
@@ -312,5 +290,6 @@
   logo.onclick = gen_img; // click to refresh
   gen_img(); // also start the program
 })() // start the program for real
+; // I heard you like semicolons
 
 // the end thank you for reading
