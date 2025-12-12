831999413ea74c111a7921e364b77e5d8d4db4b8
ritz <pseudoscalar@gmail.com>
2025-12-12 21:09:48 +0100

    animated logo on click

--- logo.js
+++ logo.js
@@ -1,8 +1,10 @@
 // Genuary 2026 logo code, Copyright by Piter Pasma
 
 (code=({copyright:piterpasma,max:G,floor:Z,abs:B,sin,cos,min:U,imul,PI}=Math,TAU=PI*2)=>{
-  gen_img = (seed="blemk"+Date.now())=>{
+  seed="ANGRUARY"+Date.now();
+  gen_img = (anim)=>{
     start_time = Date.now();
+    if(anim)seed="ANGRUARY"+Date.now();
     // init canvas
     ASPECT=logo.offsetWidth/logo.offsetHeight; // aspect ratio
     V=document.createElement`canvas`;
@@ -13,6 +15,7 @@
     PRNG=(s,a=9,b,c,d,R=(x=1)=>x*(x=d^d<<11,d=c,c=b,b=a,((a^=x^x>>>8^(b>>>19))>>>0)/2**32))=>([...s+1/0].map(e=>R(d^=e.charCodeAt()*a)),R);
     R=PRNG(seed);
     T=a=>R(a)-R(a); // triangle noise
+    RS=a=>R(2)<1?a:-a;
     L=(x,y,z=0)=>(x*x+y*y+z*z)**.5; // vec2/3 length (adapted from: Elements, Euclid 300 BC)
     N=([x,y,z=0],m=L(x,y,z))=>[x/m,y/m,z/m]; // vec3 normalize
 
@@ -25,7 +28,14 @@
     // RF=_=>`W(${F(3,_=>2**(R(.6)-.3))})`;
     RT4=_=>`X(${F(4,_=>R(TAU))})`;
     RF4=_=>`X(${F(4,i=>2**(i*.6+R(.6)-1.3))})`;
+    vRT4=_=>F(4,_=>R(TAU));
+    vRF4=_=>F(4,i=>2**(i*.6+R(.6)-1.3));
+
+    // frarg shade
     frag_shader = `out X c;
+
+    uniform X f0,f1,f2,f3,p0,p1,p2,p3;
+    uniform V zz;
 
     F aastep(F threshold, F value) {
       F afwidth = L(vec2(dFdx(value), dFdy(value))) * 0.7;
@@ -51,8 +61,8 @@
       const F ma = 3;
       X q = X(p,p.yx*K*2);
       return dot(
-          sin(q.xyzw*${RF4()}+${RT4()}+ma*sin(q.yzwx*${RF4()}+${RT4()})),
-          sin(q.zwxy*${RF4()}+${RT4()}+ma*sin(q.wxyz*${RF4()}+${RT4()}))
+          sin(q.xyzw*f0+p0+ma*sin(q.yzwx*f1+p1)),
+          sin(q.zwxy*f2+p2+ma*sin(q.wxyz*f3+p3))
         );
     }
 
@@ -86,8 +96,6 @@ F GENUARY(V p) {
   return U(G_,U(E,U(k(B(U(N,U(A,R))),3-y),U(U_,U(Y,U(d2a,U(d2b,U(d6a,d6b))))))));
 }
 
-    F pp0 = ${R(TAU)}, pp1 = ${R(TAU)}, pp2 = ${R(TAU)}, pp3 = ${R(TAU)}, pp4 = ${R(TAU)};
-
     F rf=9,
       bd = .6;
     V ni = V(17,-23), n0 = V(0,0);
@@ -104,7 +112,7 @@ F GENUARY(V p) {
 
     W col(V p) {
       // perspectiv
-      F z = dot(p,V(${[T(2),T(4)]}))+23;
+      F z = dot(p,zz)+23;
       p *= 450/z; 
       // shadow offset
       const F oo = .4;
@@ -142,7 +150,6 @@ F GENUARY(V p) {
 
     // WebGL2 init / wow very plate, such boiler
     g=V.getContext`webgl2`;p=g.createProgram(t=35633);[`out O{X x=X(-1);x[gl_VertexID]=3.;gl_Position=X(u=x.xy,0,1);u.x*=${ASPECT};}`,frag_shader].map(x=>g.attachShader(p,s=g.createShader(t--),g.shaderSource(s,src=`#version 300 es\n~V vec2~W vec3~X vec4~F float~O V u;void main()~L length~S smoothstep~U min~B abs~G max\nprecision highp F;`.split`~`.join`\n#define `+fix_GLSL(x)),g.compileShader(s)));g.linkProgram(p);g.useProgram(p);
-
     // fine, if you also want error messages
     if (!g.getProgramParameter(p,g.LINK_STATUS)) {
       // display error with (possibly generated) code and line numbers
@@ -151,15 +158,30 @@ F GENUARY(V p) {
       console.log(`S LOG:\n${g.getShaderInfoLog(s)}`);
       throw 'AARG DED';
     }
+// fpz
+    c=[];
+    un = (t,n,v)=>g[`uniform${t}v`](g.getUniformLocation(p,n),v);
 
-    g.drawArrays(5,0,3)
+
+    pp=F(4,i=>(un('4f','f'+i,vRF4()),vRT4()));
+    pd = F(16,i=>RS(1+R(3)));
+    un('2f','zz',[T(2),T(4)]);
+    k=_=>{
+      let t = performance.now() * 5e-5;
+      F(4,i=>{
+        un('4f','p'+i,pp[i].map((v,j)=>v+t * pd[i*4+j]));
+      });
+      g.drawArrays(5,0,3);
+      if(anim)requestAnimationFrame(k);
+    };
+    k();
 
     console.log(`elaps = ${Date.now() - start_time}ms`);
 
   };
-
-  logo.onclick = e=>gen_img(); // click to refresh
-  gen_img(); // start the program
+  anim=0;
+  logo.onclick = e=>gen_img(anim^=1); // click to refresh
+  gen_img(anim); // start the program
 })(); // start the program for real
 
 // the end thank you for reading
